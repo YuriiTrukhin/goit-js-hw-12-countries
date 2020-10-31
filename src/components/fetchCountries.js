a6bbb4a17c6f197484e255b6e71c3a315db4440d
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-10-31 18:27:38 +0200

    добавляет кнопку закрытия алерта

--- src/components/fetchCountries.js
+++ src/components/fetchCountries.js
@@ -1,18 +1,7 @@
 import template from "../templates/listTemplate.hbs"
-
 import {alert,info} from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
-
-// import {
-//   alert,
-//     defaultModules,
-  
-// } from "../../node_modules/@pnotify/core/dist/PNotify.js";
-// import * as PNotifyDesktop from "../../node_modules/@pnotify/desktop/dist/PNotifyDesktop.js";
-// import "../../node_modules/@pnotify/core/dist/PNotify.css";
-// // import "../../node_modules/@pnotify/desktop/dist/PNotifyDesktop.css";
-// import '../../node_modules/@pnotify/core/dist/BrightTheme.css'
-// defaultModules.set(PNotifyDesktop, {});
+import '@pnotify/core/dist/BrightTheme.css';
 
 export default function fetchCountries(searchQuery) {
     const input = document.querySelector(".input")
